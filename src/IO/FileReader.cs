b6eb7145d60af8cd38810c5c081365cff88e0d42
debbyalmadea <almadeaputri@gmail.com>
2023-03-18 21:37:55 +0700

    add: file reader and test case

---
+++ src/IO/FileReader.cs
@@ -0,0 +1,42 @@
+using Mazes;
+using System;
+using System.IO;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+
+namespace IO
+{
+    public class FileReader
+    {
+        public Maze read(string filePath)
+        {
+            Maze maze = new Maze();
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                string line;
+                // Read and display lines from the file until the end of
+                // the file is reached.
+                while ((line = sr.ReadLine()) != null)
+                {
+                    List<Tile> tiles = convertLine(line);
+                    maze.maze.Add(tiles);
+                }
+            }
+            return maze;
+        }
+
+        private List<Tile> convertLine(string line)
+        {
+            List<Tile> tiles = new List<Tile>();
+            string[] strings = line.Split(null);
+            foreach (string s in strings)
+            {
+                tiles.Add(new Tile(s.ToCharArray()[0]));
+            }
+
+            return tiles;
+        }
+    }
+}
--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -12,6 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Mazes;
+using System.IO;
+using Microsoft.Win32;
+using System.Diagnostics;
+using IO;
 
 namespace src
 {
@@ -24,5 +29,27 @@ namespace src
         {
             InitializeComponent();
         }
+
+        public void openFileButton(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                filename.Text = openFileDialog.SafeFileName;
+                FileReader fileReader = new FileReader();
+                Maze maze = fileReader.read(openFileDialog.FileName);
+                foreach (var row in maze.maze)
+                {
+                    foreach(var col in row)
+                    {
+                        Trace.Write(col.Category + " ");
+                    }
+                    Trace.WriteLine("");
+                }
+            }
+        }
+
+
     }
 }
